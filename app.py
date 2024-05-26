44cdd7f1770b0ce931aeb5e972e41875dcfc1038
BBOXEEEE <noeyhesx@naver.com>
2024-05-26 20:26:05 +0900

    feat: STT 기능 구현

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 from apps.object_detection import object_detection
+from apps.speech_to_text import speech_to_text
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(object_detection, url_prefix='/')
+app.register_blueprint(speech_to_text, url_prefix='/stt')
+
 
 address = '0.0.0.0'
 port = 15555
